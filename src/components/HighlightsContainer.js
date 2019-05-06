1b298d8be56b270bad3c2f72d790dd390c969230
dannykrug <dannykrug@gmail.com>
2019-05-06 12:47:49 -0400

    adding highlights

---
+++ src/components/HighlightsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class HighlightsContainer extends Component {
+  render(){
+    return (
+      <div>
+      <ul>
+
+      </ul>
+      </div>
+    )
+  }
+}
--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -12,6 +12,9 @@ export default class ProductContainer extends Component {
       <h1>{product.title}</h1>
       <h2>{product.Offers[0].OfferPrice[0].formattedPriceValue}</h2>
       <p>{product.Offers[0].OfferPrice[0].priceQualifier}</p>
+      <h3>returns</h3>
+      <p>This item must be returned within {product.ReturnPolicy[0].ReturnPolicyDetails[0].policyDays} days. {product.ReturnPolicy[0].ReturnPolicyDetails[0].guestMessage}.</p>
+      <h1>product highlights</h1>
       </div>
     )
   }
