3547fb97f6ad3c251d2e0742c7d89e472c0358a4
dannykrug <dannykrug@gmail.com>
2019-05-07 09:55:59 -0400

    quantity counter working

--- src/components/QuantityCounter.js
+++ src/components/QuantityCounter.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 export default class QuantityCounter extends Component {
+
+  state = { count: 1 }
+
   render() {
+    const addQuantity = (e) => {
+      e.preventDefault()
+      let newCount = this.state.count + 1
+      this.setState({
+        count: newCount
+      })
+    }
+
+    const lowerQuantity = (e) => {
+      let newCount = this.state.count - 1
+      if(newCount <= 0) {
+        this.setState({
+          count: 1
+        })
+      } else {
+        this.setState({
+          count: newCount
+        })
+      }
+    }
+
+    console.log(this.state);
     return (
       <div>
-        Quantity
+        Quantity <span onClick={lowerQuantity} id="minus">-</span> {this.state.count} <span onClick={addQuantity} id="plus">+</span>
       </div>
     )
   }
