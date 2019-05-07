f54734e4df088a969d34a5d38e4bb3f0b15f27a3
dannykrug <dannykrug@gmail.com>
2019-05-06 20:37:18 -0400

    styling started

--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -3,6 +3,10 @@ import HighlightsContainer from './HighlightsContainer'
 import PromotionContainer from './PromotionContainer'
 import PhotoCarousel from './PhotoCarousel'
 import ReviewContainer from './ReviewContainer'
+import QuantityCounter from './QuantityCounter'
+import Button from './Button'
+import ListShareButton from './ListShareButton'
+import '../App.css'
 
 export default class ProductContainer extends Component {
   render() {
@@ -11,16 +15,26 @@ export default class ProductContainer extends Component {
 
     return (
       <div>
-      <h1>{product.title}</h1>
-      <PhotoCarousel items={product.Images[0]}/>
-      <ReviewContainer reviews={product.CustomerReview[0]} />
-      <h2>{product.Offers[0].OfferPrice[0].formattedPriceValue}</h2>
-      <p>{product.Offers[0].OfferPrice[0].priceQualifier}</p>
-      <PromotionContainer promotions={product.Promotions} />
-      <h3>returns</h3>
-      <p>This item must be returned within {product.ReturnPolicy[0].ReturnPolicyDetails[0].policyDays} days. {product.ReturnPolicy[0].ReturnPolicyDetails[0].guestMessage}.</p>
-      <h1>product highlights</h1>
-      <HighlightsContainer highlights={product.ItemDescription[0].features}/>
+        <div class="split left">
+          <h1>{product.title}</h1>
+          <PhotoCarousel items={product.Images[0]}/>
+          <ReviewContainer reviews={product.CustomerReview[0]} />
+        </div>
+        <div class="split right">
+          <h2>{product.Offers[0].OfferPrice[0].formattedPriceValue}</h2>
+          <p>{product.Offers[0].OfferPrice[0].priceQualifier}</p>
+          <PromotionContainer promotions={product.Promotions} />
+          <QuantityCounter />
+          <Button name="Pickup In Store" />
+          <Button name="Add to Cart" />
+          <h3>returns</h3>
+          <p>This item must be returned within {product.ReturnPolicy[0].ReturnPolicyDetails[0].policyDays} days. {product.ReturnPolicy[0].ReturnPolicyDetails[0].guestMessage}.</p>
+          <ListShareButton name="ADD TO REGISTRY" />
+          <ListShareButton name="ADD TO LIST" />
+          <ListShareButton name="SHARE" />
+          <h1>product highlights</h1>
+          <HighlightsContainer highlights={product.ItemDescription[0].features}/>
+          </div>
       </div>
     )
   }
---
+++ src/components/QuantityCounter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class QuantityCounter extends Component {
+  render() {
+    return (
+      <div>
+        Quantity
+      </div>
+    )
+  }
+}
