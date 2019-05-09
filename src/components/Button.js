67b00e64ef4c2e874c3ff3bde615a703f803b037
dannykrug <dannykrug@gmail.com>
2019-05-09 14:03:32 -0400

    working on it

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { useAlert } from 'react-alert';
 import '../css/button.css'
 
 export default class Button extends Component {
 
   render() {
+
+    // const alert = useAlert()
+    //
+    // let seeAlert = (e) => {
+    //   e.preventDefault()
+    //   alert.show("added!")
+    // }
+
     return (
-      <div className={this.props.className} id={this.props.id}>
+      <div  className={this.props.className} id={this.props.id}>
         <p>{this.props.name}</p>
       </div>
     )
